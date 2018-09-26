b4e4bd9270b566026cb29cce47b3fcb23d227ff3
Sharon Miller <sharon@sharonmillercreative.com>
2018-09-25 22:22:06 -0700

    add schema class

--- src/models/classes.js
+++ src/models/classes.js
@@ -3,7 +3,7 @@
 import mongoose from 'mongoose';
 
 const classesSchema = mongoose.Schema({
-  className: {type:String, required:true},
+  name: {type:String, required:true},
   classPeriod: {type:Number, required:true},
   students:[{type:mongoose.Schema.Types.ObjectID, ref: 'students'}],
 });
